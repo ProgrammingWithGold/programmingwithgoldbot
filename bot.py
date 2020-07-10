ad7afb0e42c13cb91677c6d9878d8795e7a9733f
ProgrammingWithGold <68092881+ProgrammingWithGold@users.noreply.github.com>
2020-07-09 20:11:56 -0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '+')
+
+@client.event
+async def on_ready():
+    print(f'Ping is at {round(client.latency * 1000)}')
+
+@client.command()
+async def ping(ctx): 
+    await ctx.send(f'Your ping is at ---> {round(client.latency * 1000)}')
+
+client.run('NzMwOTgxMjEwNDY5NDMzNDgy.XwfZUg.90tDvhk2Hs63AjSqSVOZyzMXdeg')
